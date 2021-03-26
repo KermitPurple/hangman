20b22d805844cf75908ae9ffd194ce976c6c5be6
Shane McDonough <shanemcdman@gmail.com>
2021-03-25 19:47:26 -0700

    Create the print functions and get input

--- hangman.py
+++ hangman.py
@@ -92,17 +92,45 @@ r''' +------+
             return [line[:-1] for line in f.readlines()]
 
     def print_censored_word(self):
-        pass
+        for ch in self.word:
+            if ch == ' ':
+                print('   ', end='')
+            elif ch in self.guesses:
+                print(ch + ' ', end='')
+            else:
+                print('_ ', end='')
+        print()
 
     def print_used_characters(self):
-        pass
+        for ch in 'abcefghijklmnopqrstuvwxyz':
+            if ch in self.guesses:
+                print(' ', end='')
+            else:
+                print(ch, end='')
+        print()
+
+    def get_input(self):
+        print('Enter a key')
+        while(1):
+            ch = getch().decode('utf-8').lower()
+            if ch in self.guesses:
+                print('You already guessed that')
+            elif ch.isalpha():
+                self.guesses.add(ch)
+                if ch not in self.word:
+                    self.strikes += 1
+                break
 
     def run(self):
-        while(self.strikes < self.max_strikes):
+        while(1):
             system('cls')
             print(self.frames[self.strikes])
             self.print_censored_word()
             self.print_used_characters()
+            if(self.strikes == self.max_strikes - 1):
+                print(f'The word was {self.word}!')
+                break
+            self.get_input()
 
 if __name__ == '__main__':
     game = HangmanGame()
